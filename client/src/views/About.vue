a408cb314a7f28b0a9bada900fe88d775a0889c8
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-02-02 21:14:58 +1100

    update about view

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -5,7 +5,7 @@
         <h2 class="about-title"><small>ABOUT</small></h2>
         <h5>Last Updated on 3rd February 2021</h5>
         <div class="about-txt">
-          <p>Front End Engineer is a personal blog template designed for use by software developers. The client-side code was built with Vue, Vue Router, Vuex and Axios. The server-side code uses MongoDB and ExpressJS. Authentication is with JWT. The site is hosted on an Amazon, AWS, EC2 Ubuntu instance with an Nginx server.</p>
+          <p>Front End Engineer is a personal blog template designed for use by software developers. The client-side code was built with Vue, Vue Router, Vuex and Apollo Client. The server-side code uses MongoDB and Apollo Server. Authentication is with JWT. The site is hosted on an Amazon, AWS, EC2 Ubuntu instance with an Nginx server.</p>
           <p>The front-page styling and design, with masonry image packing and upside-down navigation, was based on the <a href="http://www.wix.com" target="_blank">www.wix.com</a> template: <a href="https://www.wix.com/demone2/graphic-design-portf" target="_blank">graphic-design-portf</a>. The remainder of the styling is Bootstrap 3, with flat-buttons and controls. Code syntax highlighting is with the <a href="http://prismjs.com" target="_blank">PrismJS</a> library using the default colour scheme.</p>
           <p>The blog template is currently populated with blogs created from the 1998 Netscape manual, 'Core Javascript Guide 1.4', which is can be viewed at <a href="https://docs.huihoo.com/javascript/CoreGuideJS14/index.html" target="_blank">CoreGuideJS14</a>.</p>
           <p>The code is available for download at: <a href="https://github.com/mmackenzie-syd/blog" target=_blank>https://github.com/mmackenzie-syd/blog</a></p>
