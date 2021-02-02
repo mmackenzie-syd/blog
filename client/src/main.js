28ac97b3d52b6e5befce0246fbbb387c09d5d780
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-02-03 09:06:22 +1100

    reset token on app load to prevent token expire error

--- client/src/main.js
+++ client/src/main.js
@@ -8,22 +8,24 @@ import './assets/css/highlight.css'; // global styles
 import ApolloClient from 'apollo-boost';
 import VueApollo from 'vue-apollo';
 
+// re-set json token in local storage on app load
+localStorage.setItem('token', '');
+
 export const defaultClient = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     fetchOptions: {
         credentials: 'include'
     },
-    // request: operation => {
-    //     if (!localStorage.token) {
-    //         localStorage.setItem('token', '');
-    //     }
-    //
-    //     operation.setContext({
-    //         headers: {
-    //             authorization: localStorage.getItem('token')
-    //         }
-    //     });
-    // },
+    request: operation => {
+        if (!localStorage.token) {
+            localStorage.setItem('token', '');
+        }
+        operation.setContext({
+            headers: {
+                authorization: localStorage.getItem('token')
+            }
+        });
+    },
     onError: ({ graphQLErrors, networkError }) => {
         if (networkError) {
             console.log('[networkError]', networkError);
